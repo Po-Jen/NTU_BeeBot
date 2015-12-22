d52fa56b6177d347258beeecc84df11c533f99f8
Megacephalo <charlyhuang@ira.e.ntu.edu.tw>
2015-12-22 17:19:36 +0800

    finish writing the prototype of the converter

--- mobile_safeguard_primitive/src/path2traj_conv.cpp
+++ mobile_safeguard_primitive/src/path2traj_conv.cpp
@@ -6,16 +6,54 @@
  * exclusively by riskrrt's controller.
  */
  
- #include <ros/ros.h>
- #include <iostream>
- 
+#include <ros/ros.h>
+#include <iostream>
+
+// Subscribe to /path topic
+#include "nav_msgs/Path.h"
+#include <geometry_msgs/Pose.h>
+#include <riskrrt/PoseTwistStamped.h>
+#include <tf/tf.h>
+
+// Calling in functions within riskrrt package
 #include <riskrrt/Trajectory.h>
 #include <riskrrt/riskrrt.hpp>
 
-
 using namespace std ;
 
-int main ()
+ros::Publisher traj_pub ;
+
+void pathCallback (const nav_msgs::Path::ConstPtr &msg){
+	riskrrt::Trajectory traj_msgs ;
+	riskrrt::PoseTwistStamped pose_twist ;
+	
+	traj_msgs.poses.clear() ;
+	
+	for (int i = 0 ; i < msg -> poses.size(); i++ ) {
+		pose_twist.pose.position.x = msg -> poses[i].pose.position.x ;
+	    pose_twist.pose.position.y = msg -> poses[i].pose.position.y ;
+	    pose_twist.pose.position.z = 0.0 ;                                   /// should be canceled if we were to do 3D planning
+	    pose_twist.pose.orientation = msg -> poses[i].pose.orientation ;
+	}
+	
+	traj_msgs.poses.push_back (pose_twist) ;
+
+	// Publish msgs into the topic
+	traj_pub.publish (traj_msgs) ;
+} 
+
+int main (int argc, char** argv)
 {
-	return 0 ;
+	// Initialize ROS
+	ros::init (argc, argv, "path2traj_conv") ;
+	ros::NodeHandle nh ;
+	
+	// create ROS subscriber
+	ros::Subscriber traj_sub = nh.subscribe ("/path", 1, pathCallback ) ;
+	
+	// create ROS publisher
+	traj_pub = nh.advertise<riskrrt::Trajectory> ("/traj", 10) ;
+	
+	// Spin
+	ros::spin() ;
 }
