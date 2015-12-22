9ec4fdd7be0ad2cf7926588ade620826a856dd6b
Megacephalo <charlyhuang@ira.e.ntu.edu.tw>
2015-12-22 14:19:01 +0800

    Add a /path to /traj converter node and fix bugs calling riskrrt header.

---
+++ mobile_safeguard_primitive/src/path2traj_conv.cpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2015 Charly Huang, National Taiwan University
+ * Dec 21, 2015 16:49
+ * 
+ * This file is intended to convert /path topic to /traj used
+ * exclusively by riskrrt's controller.
+ */
+ 
+ #include <ros/ros.h>
+ #include <iostream>
+ 
+#include <riskrrt/Trajectory.h>
+#include <riskrrt/riskrrt.hpp>
+
+
+using namespace std ;
+
+int main ()
+{
+	return 0 ;
+}
