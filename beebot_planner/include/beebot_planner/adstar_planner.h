1b6aed32210807e7b23698cb0869f8a1d45cb306
Megacephalo <charlyhuang@ira.e.ntu.edu.tw>
2016-01-21 22:37:31 +0800

    Carry on working on completing global planner plugin

--- beebot_planner/include/beebot_planner/adstar_planner.h
+++ beebot_planner/include/beebot_planner/adstar_planner.h
@@ -1,7 +1,40 @@
 /*
  * adstar_planner.h
+ * 
+ * Charly Huang, Jan 21, 2016, National Taiwan University
+ * 
+ * This is an adaption of SBPLPlanner2D to global planner plugin, but 
+ * runs only on AD* motion planning algorithm.
 */
 
+/*
+ * Copyright 2013 Armin Hornung, University of Freiburg
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Freiburg nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
 #ifndef ADSTAR_PLANNER_H
 #define ADSTAR_PLANNER_H
 
@@ -47,6 +80,12 @@
 #include <base_local_planner/world_model.h>
 #include <base_local_planner/costmap_model.h>
 
+/**SBPL elements*/
+#include <sbpl/headers.h>
+
+/**grid_map_2D elements*/
+#include <gridmap_2d/GridMap2D.h>
+
 namespace adstar_planner{
 	/**
 	 * @class adstar_planner
@@ -67,6 +106,45 @@ namespace adstar_planner{
 		     ADStarPlanner (std::string name, costmap_2d::Costmap2DROS* costmap_ros) ;
 		     
 		     /**
+		      * Destructor
+		      */
+		     virtual ~ADStarPlanner () ;  
+		     
+		     /**
+		      * @function goalCallback(goal)
+		      * @param goal
+		      * @brief Set goal and plan when start was already set
+		      */
+		     void goalCallback(const geometry_msgs::PoseStampedConstPtr& goal_pose) ;
+		      
+		     /**
+		       * @function startCallback(start)
+		       * @param start
+		       * @brief Set start and plan when goal was already set
+		       */
+		     void startCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& start_pose) ; 
+		     
+		     /**
+		      * @function mapCallback(map)
+		      * @param occupancy_map
+		      * @brief Calls updateMap ()
+		      */   
+		     void mapCallback(nav_msgs::OccupancyGridConstPtr& occupancy_map) ;
+		     
+		     /**
+		      * @function updateMap (map)
+		      * @param map The map in grid_map_2D format
+		      * @brief Setup the internal map representation and initialize the SBPL planning environment
+		      */ 
+		     bool updateMap(gridmap_2d::GridMap2DPtr map) ; 
+		     
+		     /**
+		      * @function getMap()
+		      * @brief using grid_map_2D API to transform the map from occupancy grid to 2D grid
+		      */ 
+		     gridmap_2d::GridMap2DPtr getMap() const { return map_;};
+		      
+		     /**
 		      * @function initialize (name, costmap_ros)
 		      * @brief Initialization function for the Carrot Planner
 		      * @param 
@@ -81,13 +159,62 @@ namespace adstar_planner{
 		      * @param plan The plan filled by the planner
 		      * @return True if a a valid plan was found, false otherwise
 		      */ 
-		      bool makePlan (const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan) ;
-		      
+		     bool makePlan(const geometry_msgs::PoseStamped& start, 
+                        const geometry_msgs::PoseStamped& goal, 
+                        std::vector<geometry_msgs::PoseStamped>& plan) ;
+		     
+		     /**
+		      * @function plan (start, goal)
+		      * @function plan(startX, startY, goalX, goalY)
+		      * @param start
+		      * @param goal
+		      * @param startX, startY
+		      * @param goalX, goalY
+		      * @brief makePlan calls the plan(.) function to solve the motion planning problem. Here, both functions with arguments load in the value
+		      * and the private method plan() carries on the actual work.
+		      */  
+		     bool plan (const geometry_msgs::Pose& start, const geometry_msgs::Pose& goal) ;
+		     bool plan (double startX, double startY, double goalX, double goalY) ;
+		     
+		     /**
+		      * @function getPathCosts()
+		      * @brief return costs of the path (=length in m), if planning was successful 
+		      */ 
+		     inline double getPathCosts() const {return path_costs_ ;} ; 
+		     
+		     inline const nav_msgs::Path& getPath() const {return path_ ;} ;
+		     inline double getRobotRadius() const {return robot_radius_ ;} ;
+		     
 		  private:
+		   
 		      costmap_2d::Costmap2DROS* costmap_ros_ ;
 		      double step_size_, min_dist_from_robot_ ;
 		      costmap_2d::Costmap2D* costmap_ ;
 		      base_local_planner::WorldModel* world_model_ ;    ///@brief The world model that the controller will use
+		      
+		      bool plan() ;
+		      void setPlanner() ;  /// (re)sets the planner
+		      ros::Subscriber goal_sub_, start_sub_, map_sub_ ;
+		      ros::Publisher path_pub_ ;
+		      boost::shared_ptr<SBPLPlanner> planner_ ;                   
+		      boost::shared_ptr<EnvironmentNAV2D> planner_environment_ ;
+		      gridmap_2d::GridMap2DPtr map_;
+		      
+		      std::string planner_type_ ;
+		      double allocated_time_ ;
+		      double initial_epsilon_ ;
+		      bool search_until_first_solution_ ;
+		      bool forward_search_ ;
+		      double robot_radius_ ;
+		      
+		      bool start_received_, goal_received_ ;
+		      geometry_msgs::Pose start_pose_, goal_pose_ ;
+		      nav_msgs::Path path_ ;
+		      double path_costs_ ;
+		      
+		      static const unsigned char OBSTACLE_COST = 20 ;
+		      
+		      bool initialized_ ;
 		      
 		      /**
 		       * @function footprintCost (x_i, y_i, theta_i)
@@ -98,8 +225,6 @@ namespace adstar_planner{
 		       * @return
 		       */ 
 		      double footprintCost(double x_i, double y_i, double theta_i) ;
-		      
-		      bool initialized_ ;
 	 } ;
 }
 
--- beebot_planner/src/adstar_planner.cpp
+++ beebot_planner/src/adstar_planner.cpp
@@ -29,6 +29,16 @@ PLUGINLIB_EXPORT_CLASS(adstar_planner::ADStarPlanner, nav_core::BaseGlobalPlanne
 			  private_nh.param("step_size", step_size_, costmap_ -> getResolution()) ;
 			  private_nh.param("min_dist_from_robot", min_dist_from_robot_, 0.10) ;  // This number can be changed
 			  world_model_ = new base_local_planner::CostmapModel(*costmap_) ;
+
+			  private_nh.param("planner_type", planner_type_, std::string("ADPlanner"));
+			  private_nh.param("search_until_first_solution", search_until_first_solution_, false);
+			  private_nh.param("allocated_time", allocated_time_, 7.0);
+			  private_nh.param("forward_search", forward_search_, false);
+			  private_nh.param("initial_epsilon", initial_epsilon_, 3.0);
+			  private_nh.param("robot_radius", robot_radius_, robot_radius_);
+
+			  path_pub_ = private_nh.advertise<nav_msgs::Path>("planned_path", 0);						  
+			  
 			  
 			  initialized_ = true ;
 		  }
@@ -52,43 +62,192 @@ PLUGINLIB_EXPORT_CLASS(adstar_planner::ADStarPlanner, nav_core::BaseGlobalPlanne
 		 double footprint_cost = world_model_ -> footprintCost(x_i, y_i, theta_i, footprint) ;
 		 return footprint_cost ;
 	 }
-	 
-	 bool ADStarPlanner::makePlan (const geometry_msgs::PoseStamped& start,
-	                               const geometry_msgs::PoseStamped& goal,
-	                               std::vector<geometry_msgs::PoseStamped>& plan){
-			if (!initialized_){
-				ROS_ERROR("The planner has not been initialized. Please call initialize() to use the planner") ;
-				return false ;
-			}
-			
-			ROS_DEBUG("Got a start: %.2f, %.2f, and a goal: %.2f, %.2f", 
-			          start.pose.position.x, start.pose.position.y,
-			          goal.pose.position.x, goal.pose.position.y) ;
-			
-			plan.clear() ;
-			costmap_ = costmap_ros_ -> getCostmap() ;
 
-			if(goal.header.frame_id != costmap_ros_->getGlobalFrameID()){
-				ROS_ERROR("This planner as configured will only accept goals in the %s frame, but a goal was sent in the %s frame.", 
-				costmap_ros_->getGlobalFrameID().c_str(), goal.header.frame_id.c_str());
-				return false;
-			}
-			
-			tf::Stamped<tf::Pose> goal_tf ;
-			tf::Stamped<tf::Pose> start_tf ;
-			
-			poseStampedMsgToTF(goal, goal_tf) ;
-			poseStampedMsgToTF(start, start_tf) ;			
-			
-			bool done = false ;
-			/**
-			 * @TODO 
-			 */ 
-			 if(!done){
-			 }
-			 
-			return (done);
-				
-	 }
+	void ADStarPlanner::goalCallback(const geometry_msgs::PoseStampedConstPtr& goal_pose){
+		//set goal
+		goal_pose_ = goal_pose->pose;
+		goal_received_ = true ;
+		ROS_DEBUG("Received goal: %f %f", goal_pose_.position.x, goal_pose_.position.y) ;
+		if (goal_pose->header.frame_id != map_->getFrameID()){
+			ROS_WARN("Goal pose frame id \"%s\" different from map frame id \"%s\"", goal_pose->header.frame_id.c_str(), map_->getFrameID().c_str());
+		}
+  
+	  // planning?
+	  if (start_received_)
+		plan();		
+	}
+		      
+	void ADStarPlanner::startCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& start_pose){
+	  // set start:
+	  start_pose_ = start_pose->pose.pose;
+	  start_received_ = true;
+	  ROS_DEBUG("Received start: %f %f", start_pose_.position.x, start_pose_.position.y);
+
+	  if (start_pose->header.frame_id != map_->getFrameID()){
+		ROS_WARN("Start pose frame id \"%s\" different from map frame id \"%s\"", start_pose->header.frame_id.c_str(), map_->getFrameID().c_str());
+	  }
+	  
+	  // planning?
+	  if (goal_received_)
+		plan();		
+	}
+		     
+	void ADStarPlanner::mapCallback(nav_msgs::OccupancyGridConstPtr& occupancy_map){
+	  gridmap_2d::GridMap2DPtr map(new gridmap_2d::GridMap2D(occupancy_map));
+	  updateMap(map);		
+	}
+		     
+	bool ADStarPlanner::updateMap(gridmap_2d::GridMap2DPtr map){
+	  planner_environment_.reset(new EnvironmentNAV2D());
+	  planner_environment_->InitializeEnv(int(map->getInfo().width), int(map->getInfo().height), 0, OBSTACLE_COST);
+	  // environment is set up, reset planner:
+	  setPlanner();
+
+	  // store local copy:
+	  map_.reset(new gridmap_2d::GridMap2D(*map));
+	  map_->inflateMap(robot_radius_);
+
+
+	  for(unsigned int j = 0; j < map_->getInfo().height; ++j){
+		for(unsigned int i = 0; i < map_->getInfo().width; ++i){
+		  if (map_->isOccupiedAtCell(i,j))
+			planner_environment_->UpdateCost(i, j, OBSTACLE_COST);
+		  else
+			planner_environment_->UpdateCost(i,j,0);
+
+		}
+	  }
+
+	  ROS_DEBUG("Map set");
+
+	  return true;		
+	}
+	
+	// TODO	     	 
+	bool ADStarPlanner::makePlan(const geometry_msgs::PoseStamped& start, 
+                  const geometry_msgs::PoseStamped& goal, 
+                  std::vector<geometry_msgs::PoseStamped>& plan){
+		  if(!initialized_){
+			ROS_ERROR("Global planner is not initialized");
+			return false;
+		  }
+
+		  plan.clear();				
+		  
+		  ROS_DEBUG("adstar_planner is getting fresh copy of costmap") ;
+		  
+		  ROS_INFO("adstar_planner getting start point (%g, %g) goal point (%g, %g)",
+		           start.pose.position.x, start.pose.position.y, goal.pose.position.x, goal.pose.position.y) ;
+          
+          // Do the verification for path planning
+          return plan() ;
+    }      
+   
+   bool ADStarPlanner::plan(const geometry_msgs::Pose& start, const geometry_msgs::Pose& goal){
+	  start_pose_ = start;
+	  goal_pose_ = goal;
+
+	  start_received_ = true;
+	  goal_received_ = true;
+
+	  return plan();	   
+   }
+   
+   bool ADStarPlanner::plan(double startX, double startY, double goalX, double goalY){
+	  start_pose_.position.x = startX;
+	  start_pose_.position.y = startY;
+
+	  goal_pose_.position.x = goalX;
+	  goal_pose_.position.y = goalY;
+
+	  start_received_ = true;
+	  goal_received_ = true;
+
+	  return plan();	   
+   } 
+   
+   bool ADStarPlanner::plan() {
+		  path_.poses.clear();
+
+		  if (!map_){
+			ROS_ERROR("Map not set");
+			return false;
+		  }
+
+		  unsigned start_x, start_y, goal_x, goal_y;
+		  if (!map_->worldToMap(start_pose_.position.x, start_pose_.position.y, start_x, start_y)){
+			ROS_ERROR("Start coordinates out of map bounds");
+			return false;
+		  }
+		  if (!map_->worldToMap(goal_pose_.position.x, goal_pose_.position.y, goal_x, goal_y)){
+			ROS_ERROR("Goal coordinates out of map bounds");
+			return false;
+		  }
+
+		  if (map_->isOccupiedAtCell(start_x, start_y)){
+			ROS_ERROR("Start coordinate (%f %f) is occupied in map", start_pose_.position.x, start_pose_.position.y);
+			return false;
+		  }
+		  if (map_->isOccupiedAtCell(goal_x, goal_y)){
+			ROS_ERROR("Goal coordinate (%f %f) is occupied in map", goal_pose_.position.x, goal_pose_.position.y);
+			return false;
+		  }
+
+		  int start_id = planner_environment_->SetStart(start_x, start_y);
+		  int goal_id = planner_environment_->SetGoal(goal_x, goal_y);
+
+		  if (start_id < 0 || planner_-> set_start(start_id) == 0){
+			ROS_ERROR("Failed to set start state");
+			return false;
+		  }
+
+		  if (goal_id < 0 || planner_->set_goal(goal_id) == 0){
+			ROS_ERROR("Failed to set goal state");
+			return false;
+		  }
+
+		  // set planner params:
+		  planner_->set_initialsolution_eps(initial_epsilon_);
+		  planner_->set_search_mode(search_until_first_solution_);
+		  std::vector<int> solution_stateIDs;
+		  int solution_cost;
+
+
+		  if(planner_->replan(allocated_time_, &solution_stateIDs, &solution_cost))
+			ROS_DEBUG("Solution found. Costs: %d;  final eps: %f", solution_cost, planner_->get_final_epsilon());
+		  else{
+			ROS_INFO("Solution not found");
+			return false;
+		  }
+
+		  // scale costs (SBPL uses mm and does not know map res)
+		  path_costs_ = double(solution_cost) / ENVNAV2D_COSTMULT * map_->getResolution();
+
+		  // extract / publish path:
+		  path_.poses.reserve(solution_stateIDs.size());
+		  path_.header.frame_id = map_->getFrameID();
+		  path_.header.stamp = ros::Time::now();
+
+		  geometry_msgs::PoseStamped pose;
+		  pose.header = path_.header;
+		  for (size_t i = 0; i < solution_stateIDs.size(); i++) {
+			int mx, my;
+			planner_environment_->GetCoordFromState(solution_stateIDs[i], mx, my);
+			//ROS_INFO("p: %d - [%d %d]", solution_stateIDs[i], mx, my);
+			double wx,wy;
+			map_->mapToWorld(mx,my,wx,wy);
+
+
+			pose.pose.position.x = wx;
+			pose.pose.position.y = wy;
+			pose.pose.position.z = 0.0;
+			path_.poses.push_back(pose);
+		  }
+
+		  path_pub_.publish(path_);
+
+		  return true;	   
+   }
+	
  }
  #endif
