db0c4ccc4b42f60d3d9433a9d8448c944c7d111b
Megacephalo <charlyhuang@ira.e.ntu.edu.tw>
2016-01-27 23:27:32 +0800

    Successfully adpt and compile sampling-based global planner

---
+++ beebot_planners_master/beebot_rrt_planner/include/beebot_rrt_planner/cell.cpp
@@ -0,0 +1,15 @@
+
+
+#include <beebot_rrt_planner/cell.h>
+
+
+
+Cell::Cell(double x, double y, double w, double h) {
+    hasAgent = false;
+    occupied = false;
+}
+
+Cell::~Cell() {
+    // clean up
+
+}
